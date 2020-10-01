aefbdbee4445290a64510723bd635a58eb2b659f
Mario <martzm4118@gmail.com>
2020-10-01 08:06:15 -0500

    Nombre_m

--- PruebaVent.java
+++ PruebaVent.java
@@ -25,6 +25,7 @@ public class PruebaVent {
         JFrame ventanaM = new JFrame(titulo);
         ventanaM.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//finaliza el programa cuando se da click en la X
         ventanaM.setSize(290, 350);//configurando tamaño de la ventana
-        ventanaM.setVisible(true);//configurando visualización de la ventana        
+        ventanaM.setVisible(true);//configurando visualización de la ventana    
+        System.out.println("Mario Mtz");    
     }    
 }
